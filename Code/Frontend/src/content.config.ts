dd66eb9df6528b990a54525893a9ac8bac42d318
Adam Prendergast <adam.prenders@gmail.com>
2025-04-23 13:28:50 +0100

    Implemented Blog Collection in Content Layer

--- Code/Frontend/src/content.config.ts
+++ Code/Frontend/src/content.config.ts
@@ -1,5 +1,8 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { getAllContentByType } from "./services/ContentService";
+import type { ZodType } from "astro/zod";
+import type { BlogArticlePageContentModel } from "./umbraco-client/model";
 
 const workshopExercise = defineCollection({
     loader: glob({pattern: "**/*.md", base: "./src/data/workshop/exercises"}),
@@ -9,4 +12,21 @@ const workshopExercise = defineCollection({
       })
 })
 
-export const collections = { workshopExercise };
+const blog = defineCollection({
+    loader: async () => {
+        const response = await getAllContentByType('blogArticlePage');
+        return response.map((article) => ({
+            id: article.id,
+            slug: article.route.path,
+            content: article,
+        }));
+    },
+    schema: z.object({
+        slug: z.string(),
+        content: z.any() as ZodType<BlogArticlePageContentModel>, // An 'anything goes' Zod type that we cast to our genertated interface
+    })
+})
+
+
+
+export const collections = { workshopExercise, blog };
