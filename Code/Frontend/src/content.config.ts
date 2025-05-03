9bd343370f26b93d6948a41c1b6a087c9fcfeffa
Adam Prendergast <adam.prenders@gmail.com>
2025-05-03 15:19:28 +0100

    Open Content Config

--- Code/Frontend/src/content.config.ts
+++ Code/Frontend/src/content.config.ts
@@ -1,8 +1,5 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
-import { getAllContentByType } from './services/ContentService';
-import type { ZodType } from 'astro/zod';
-import type { BlogArticlePageContentModel } from './umbraco-client/model';
 
 const workshopExercise = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/data/workshop/exercises' }),
@@ -12,19 +9,4 @@ const workshopExercise = defineCollection({
   }),
 });
 
-const blog = defineCollection({
-  loader: async () => {
-    const response = await getAllContentByType('blogArticlePage');
-    return response.map((article) => ({
-      id: article.id,
-      slug: article.route.path,
-      content: article,
-    }));
-  },
-  schema: z.object({
-    slug: z.string(),
-    content: z.any() as ZodType<BlogArticlePageContentModel>, // An 'anything goes' Zod type that we cast to our genertated interface
-  }),
-});
-
-export const collections = { workshopExercise, blog };
+export const collections = { workshopExercise };
