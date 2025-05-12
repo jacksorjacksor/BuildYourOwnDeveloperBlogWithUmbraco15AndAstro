b560fca9263a557ba86482c0596c035650fd8219
Adam Prendergast <adam.prenders@gmail.com>
2025-05-12 13:22:23 +0100

    Checkpoint - Start - Further updates

--- Code/Frontend/src/content.config.ts
+++ Code/Frontend/src/content.config.ts
@@ -1,8 +1,10 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
-import { getAllContentByType } from './services/ContentService';
-import type { ZodType } from 'astro/zod';
-import type { BlogArticlePageContentModel } from './umbraco-client/model';
+
+// Imports for Exercise 5
+//import { getAllContentByType } from './services/ContentService';
+//import type { ZodType } from 'astro/zod';
+//import type { BlogArticlePageContentModel } from './umbraco-client/model';
 
 const workshopExercise = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/data/workshop/exercises' }),
@@ -13,19 +15,6 @@ const workshopExercise = defineCollection({
   }),
 });
 
-const blog = defineCollection({
-  loader: async () => {
-    const response = await getAllContentByType('blogArticlePage');
-    return response.map((article) => ({
-      id: article.id,
-      slug: article.route.path,
-      content: article,
-    }));
-  },
-  schema: z.object({
-    slug: z.string(),
-    content: z.any() as ZodType<BlogArticlePageContentModel>, // An 'anything goes' Zod type that we cast to our genertated interface
-  }),
-});
+// Exercise 5 - Implement Blog Content Collection here
 
 export const collections = { workshopExercise, blog };
